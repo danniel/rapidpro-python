9c810a309f9ce6b39d897fef06c053830b4c79f4
Daniel Ursache Dogariu <contact@danniel.net>
2022-11-08 13:45:27 +0200

    Add missing field

--- temba_client/v2/types.py
+++ temba_client/v2/types.py
@@ -221,6 +221,7 @@ class Message(TembaObject):
     created_on = DatetimeField()
     sent_on = DatetimeField()
     modified_on = DatetimeField()
+    attachments = ObjectListField(item_class=ObjectRef)
     media = SimpleField()
     archived = SimpleField()
 
